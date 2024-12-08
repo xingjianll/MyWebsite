302f062d1aaabd463937869bc6b659de3ee2b715
xingjianll <4396kevinliu@gmail.com>
2024-12-08 06:15:34 -0500

    chore: update styles

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -23,31 +23,25 @@ const NavBar: React.FC = () => {
         };
     }, []);
 
-    // const navbarStyle = {
-    //     backdropFilter: `blur(${Math.min(2 + Math.max(scrollY, 0)*8 / 150, 10)}px)`,
-    //     marginTop: `${Math.max(0.8 - Math.max(scrollY, 0)*0.8 / 150, 0)}rem`,
-    //     width: `calc(100% - ${Math.max(2 - Math.max(scrollY, 0)*2 / 150, 0)}rem)`, // Adjust width to 100% when scrolled more than 50px
-    //     borderRadius: `${Math.max(50 - Math.max(scrollY, 0)*50 / 150, 0)}px`,
-    //     backgroundColor: `rgba(255, 255, 255, ${Math.min(0.5 + Math.max(scrollY, 0)*0.3 / 150, 0.8)})`,
-    // };
+    const navbarStyle = {
+        backdropFilter: `blur(${Math.min(Math.max(scrollY, 0)*8 / 150, 10)}px)`
+    };
 
     return (
-        <nav className={styles.navbar}>
+        <nav className={styles.navbar} style={navbarStyle}>
             <div className={styles.links}>
                 <Link href="/" passHref className={styles.link}>
-                    HOME
+                    Home
                 </Link>
                 <Link href="/blog" passHref className={styles.link}>
-                    BLOGS
+                    Blogs
                 </Link>
-            </div>
-            <div className={styles.icons}>
-                <a href="https://www.linkedin.com/in/xingjian-liu-b87a841a4/" target="_blank" rel="noopener noreferrer">
-                    <Image src="/linkedin.svg" alt="LinkedIn" width={40} height={40} />
-                </a>
-                <a href="https://github.com/xingjianll" target="_blank" rel="noopener noreferrer">
-                    <Image src="/github.svg" alt="GitHub" width={35} height={35} />
-                </a>
+                <Link href="https://www.linkedin.com/in/xingjian-liu-b87a841a4/" passHref className={styles.link}>
+                    Linkedin
+                </Link >
+                <Link href="https://github.com/xingjianll" passHref className={styles.link}>
+                    Github
+                </Link>
             </div>
         </nav>
     );
