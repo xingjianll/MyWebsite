1e388b0f5ef6cd91b61e7881fc4b8dc4897b8197
xingjianll <4396kevinliu@gmail.com>
2025-02-11 20:16:58 -0500

    dev: remove edge fade

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -8,30 +8,19 @@ import inori2 from '../../../public/inori_figure.png'
 import {min} from "@floating-ui/utils";
 import ParticlesComponent from "@/components/Particles";
 
-interface BackgroundProps {
-    pullAmount: number;
-    maxPull: number;
-}
 
-const Background: React.FC<BackgroundProps> = ({ pullAmount, maxPull }) => {
-    const pullRatio = min(pullAmount / maxPull, 1);
-    const maskPercent = (1 - pullRatio/4) * 100;
-
-    const containerStyle = {
-        '--fade-start': `${maskPercent}%`
-    } as React.CSSProperties;
-
+const Background: React.FC = () => {
     return (
         <div className={styles.container}>
             <ParticlesComponent id={'hi'} opacity={0.3} z={2} b_opacity={0} amount={30}></ParticlesComponent>
-            <div className={styles.background} style={containerStyle}>
+            <div className={styles.background}>
                 <Image
                     src={inori1}
                     alt="character"
                     style={{width: 'auto', height: '100vh'}}
                 />
             </div>
-            <div className={styles.foreground} style={containerStyle}>
+            <div className={styles.foreground}>
                 <Image
                     src={inori2}
                     alt="character"
